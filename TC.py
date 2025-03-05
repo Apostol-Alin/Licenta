13ee066b11140f388021a7c9423d20c25c07af53
aapostol <alin-constantin.apostol@s.unibuc.ro>
2025-03-05 20:29:16 +0200

    more progress

--- TC.py
+++ TC.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-import RSAGGen
 import hashlib
 import random
 from CCA_encryption import OneTimeKeyDeterministicAE
@@ -7,18 +6,18 @@ from PublicParameters import PublicParameters
 import TTD
 import time
 
-class Opening:
+class TCOpening:
     def __init__(self, opening_value, mode):
         self.opening_value = opening_value
         self.mode = mode # mode is either "COMMITER" or "FORCE"
 
-class timed_commitment:
+class TimedCommitment:
     def __init__(self, ct: bytes, H: int):
         self.ct = ct
         self.H = H
 
     @staticmethod
-    def commit(message: bytes) -> tuple[timed_commitment, Opening]:
+    def commit(message: bytes) -> tuple[TimedCommitment, TCOpening]:
         alpha = random.randint(1, pow(2, PublicParameters.lambda_))
         H = pow(PublicParameters.h, alpha, PublicParameters.N)
         Z = pow(PublicParameters.z, alpha, PublicParameters.N)
@@ -27,21 +26,29 @@ class timed_commitment:
         t = t.to_bytes((t.bit_length() + 7) // 8 or 1)
         ct = OneTimeKeyDeterministicAE.encrypt(enc_key, message, t)
         # Use the time parameter as additional data
-        return timed_commitment(ct, H), Opening(alpha, "COMMITER")
+        return TimedCommitment(ct, H), TCOpening(alpha, "COMMITER")
 
     @staticmethod
-    def force_open(tc: timed_commitment) -> tuple[bytes, Opening]:
+    def force_open(tc: TimedCommitment) -> tuple[bytes, TCOpening]:
         Z = pow(tc.H, pow(2, PublicParameters.t), PublicParameters.N)
+        # TODO: add proof of exponentation
         enc_key = hashlib.sha256(str(Z).encode('utf-8')).digest()
         t = PublicParameters.t
         t = t.to_bytes((t.bit_length() + 7) // 8 or 1)
         message = OneTimeKeyDeterministicAE.decrypt(enc_key, tc.ct, t)
-        return message, Opening(Z, "FORCE")
+        return message, TCOpening(Z, "FORCE")
     @staticmethod
-    def verify_opening(message: bytes, tc: timed_commitment, opening: Opening):
+    def verify_opening(message: bytes, tc: TimedCommitment, opening: TCOpening):
         if opening.mode == 'FORCE':
+            Z = opening.opening_value
+            enc_key = hashlib.sha256(str(Z).encode('utf-8')).digest() # use the same hash function to obtain encryption key
+            t = PublicParameters.t
+            t = t.to_bytes((t.bit_length() + 7) // 8 or 1)
+            pt = OneTimeKeyDeterministicAE.decrypt(enc_key, tc.ct, t)
+            # TODO: add proof of exponentiation verification
+            if str(pt).encode('utf-8') != str(message).encode('utf-8'):
+                return False
             return True
-            print("COMMITER")
         elif opening.mode == 'COMMITER':
             alpha = opening.opening_value # here we receive as opening value the random value used to compute Z
             Z = pow(PublicParameters.z, alpha, PublicParameters.N) # we calculate Z based on received alpha
@@ -62,12 +69,12 @@ class timed_commitment:
 
 if __name__ == "__main__":
     lambda_ = 256
-    t = pow(2, 27)
+    t = pow(2, 25)
     RSAG, z = TTD.setup(lambda_, t)
     PublicParameters(lambda_, t, RSAG.h, RSAG.g, RSAG.N, z)
     message = bytes("Hello, Alin!".encode('utf-8'))
-    commitment, opening = timed_commitment.commit(message)
-    print(timed_commitment.verify_opening(message, commitment, opening))
+    commitment, opening = TimedCommitment.commit(message)
+    print(TimedCommitment.verify_opening(message, commitment, opening))
 
     # alpha = opening.opening_value
     # H = pow(RSAG.h, alpha, RSAG.N)
@@ -82,10 +89,11 @@ if __name__ == "__main__":
     # t = t.to_bytes((t.bit_length() + 7) // 8 or 1)
     # pt = OneTimeKeyDeterministicAE.decrypt(enc_key, commitment.ct, t)
     # print(pt)
-    # start_force = time.time()
-    # print("Started force opening...")
-    # forced_message, forced_opening = timed_commitment.force_open(commitment)
-    # print(f"Force opening time duration: {time.time() - start_force} seconds")
+    start_force = time.time()
+    print("Started force opening...")
+    forced_message, forced_opening = TimedCommitment.force_open(commitment)
+    print(f"Force opening time duration: {time.time() - start_force} seconds")
+    print(TimedCommitment.verify_opening(forced_message, commitment, forced_opening))
     # print(str(forced_message))
     # print(pow(PublicParameters.z, opening.opening_value, PublicParameters.N))
     # print(forced_opening.opening_value)
--- TTD.py
+++ TTD.py
@@ -3,31 +3,64 @@ import RSAGGen
 import hashlib
 import random
 from CCA_encryption import OneTimeKeyDeterministicAE
+import PublicParameters
+import TC
 
-def pedersen_commit(RSAG, message):
-    alpha = int(random.randint(1, RSAG.N - 1))
-    commitment = (pow(RSAG.g, message, RSAG.N) * pow(RSAG.h, alpha, RSAG.N)) % RSAG.N
+def pedersen_commit(message: int) -> tuple[int, int]:
+    alpha = int(random.randint(1, PublicParameters.N - 1))
+    commitment = (pow(PublicParameters.g, message, PublicParameters.N) * pow(PublicParameters.h, alpha, PublicParameters.N)) % PublicParameters.N
     return commitment, alpha
 
-def pedersen_open(RSAG, commitment, message, opening):
-    return commitment ==  (pow(RSAG.g, message, RSAG.N) * pow(RSAG.h, opening, RSAG.N)) % RSAG.N
+def pedersen_open(commitment: int, message: int, opening: int) -> bool:
+    return commitment ==  (pow(PublicParameters.g, message, PublicParameters.N) * pow(PublicParameters.h, opening, PublicParameters.N)) % PublicParameters.N
 
 def setup(lambda_, t):
     RSAG = RSAGGen.RSAG(lambda_, t)
     # euler's totient function - totient(N) = (q1-1)(q2-1) since we know that N = q1 * q2 where q1 and q2 are prime numbers
     z = pow(RSAG.h, pow(2, RSAG.t, (RSAG.q1 - 1) * (RSAG.q2 - 1)), RSAG.N)
+    PublicParameters.PublicParameters(lambda_, t, RSAG.h, RSAG.g, RSAG.N, RSAG.z)
     return RSAG, z
 
-def TTD_commit(RSAG, message, z):
-    commitment, opening = pedersen_commit(RSAG, message)
-    alpha = random.randint(1, pow(2, RSAG.lambda_))
-    H = pow(RSAG.h, alpha, RSAG.N)
-    Z = pow(z, alpha, RSAG.N)
-    enc_key = hashlib.sha256(str(Z).encode('utf-8')).hexdigest().encode('utf-8')
-    ct = OneTimeKeyDeterministicAE.encrypt(enc_key, bytes(message), RSAG.t)
-    return (commitment, )
+def TTD_commit(message: int):
+    ped_commitment, ped_opening = pedersen_commit(message)
+    # need to time commit the pair (message, ped_opening)
+    # both are int -> represent them as hexadecimal values
+    # append them togheter in a string
+    # bytes(result)
+    # time commit the bytes
 
-# if __name__ == "__main__":
-#     lambda_ = 256
-#     RSAG, z = setup(lambda_, 3)
-#     TTD_commit(RSAG, 12, z)
+    hex_message = str(hex(message))
+    hex_ped_opening = str(hex(ped_opening))
+    val = f"0x{hex_message[2:]}{hex_ped_opening[2:]}"
+    val = bytes(val.encode('utf-8'))
+
+    tc, tc_opening = TC.TimedCommitment.commit(val)
+
+    return (ped_commitment, ped_opening), (tc, tc_opening)
+
+def TTD_verify_opening(message: bytes, tc: TC.TimedCommitment, opening: TC.TCOpening):
+    valid_tc = TC.TimedCommitment.verify_opening(message, )
+
+if __name__ == "__main__":
+    a = 23123
+    b = 90323    
+    a = hex(a)
+    b = hex(b)
+    str_a = str(a)[2:]
+    str_b = str(b)[2:]
+    print(str_a)
+    print(str_b)
+    comb = f"0x{str_a}{str_b}"
+    print(bytes(comb.encode('utf-8')))
+    bytes_comb = bytes(comb.encode('utf-8'))
+    back_to_string = bytes_comb.decode()[2:]
+    print(back_to_string)
+    back_to_a = back_to_string[:len(str_a)]
+    back_to_b = back_to_string[len(str_a):len(str_a) + len(str_b)]
+    print(back_to_a)
+    print(back_to_b)
+    print(int(back_to_a, 16))
+    print(int(back_to_b, 16))
+
+
+    
